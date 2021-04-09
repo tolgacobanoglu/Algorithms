b318a0b78c3894c457e5fd48db62fb06c28e7c9d
Tolga Çobanoğlu <76070986+tolgacobanoglu@users.noreply.github.com>
2021-04-09 15:49:26 +0300

    Add files via upload

--- Greedy Algorithm/main.cpp
+++ Greedy Algorithm/main.cpp
@@ -1,8 +1,8 @@
 #include <iostream>
 
-int coinChanger(int coinAmount,int index) //Greedy algorithm for coin change to minimum
+int coinChanger(double coinAmount,int index) //Greedy algorithm for coin change to minimum coin number
 {                                         //If you want to start cashing from where you can increase the index
-    int coinType[]={25,10,5,1};
+    double coinType[]={1,0.5,0.25,0.1,0.05,0.01};
     int coinNumber=0;
     while (coinAmount != 0)
     {
@@ -19,17 +19,18 @@ int coinChanger(int coinAmount,int index) //Greedy algorithm for coin change to 
     return coinNumber;
 }
 
-void infoMessage(int coinAmount,int index)//display of methods work with which money method trade with them
+void infoMessage(double coinAmount,int index)//display of methods work with which money method trade with them
 {
-    int coinType[]={25,10,5,1};
+    double coinType[]={1,0.5,0.25,0.10,0.050,0.010};
     int coinNumber=0;
     while (coinAmount != 0)
     {
+
         if (coinAmount>=coinType[index])
         {
             coinAmount-=coinType[index];
             coinNumber+=1;
-            printf("+%d$\n",coinType[index]);
+            printf(" +%.2f $\n",coinType[index]);
         }
         else
         {
@@ -40,7 +41,7 @@ void infoMessage(int coinAmount,int index)//display of methods work with which m
 
 int main()
 {
-    infoMessage(66,0);
-    printf("%d",coinChanger(66,0));
+    infoMessage(9.75,0);
+    printf("%d",coinChanger(9.75,0));// give coin number
     return 0;
 }
